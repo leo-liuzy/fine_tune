8aea905badf6ac591262dd3da9fc564386fe5f2f
leo-liuzy <zeyuliu2@cs.washington.edu>
2019-10-31 16:20:25 -0700

    add reweighting for fine-tuning

--- runs/run_squad.py
+++ runs/run_squad.py
@@ -493,6 +493,7 @@ def main():
     args.model_type = args.model_type.lower()
     config_class, model_class, tokenizer_class = MODEL_CLASSES[args.model_type]
     config = config_class.from_pretrained(args.config_name if args.config_name else args.model_name_or_path)
+    config.output_hidden_states = True  # TODO: experiment weighting different layers(ELMo style)
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
                                                 do_lower_case=args.do_lower_case)
     model = model_class.from_pretrained(args.model_name_or_path, from_tf=bool('.ckpt' in args.model_name_or_path),
