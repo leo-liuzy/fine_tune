96bae134a7f8f85c4683e8c460b4640c3290a6b4
leo-liuzy <zeyuliu2@cs.washington.edu>
2019-11-03 23:34:51 -0800

    update

--- runs/run_squad.py
+++ runs/run_squad.py
@@ -504,7 +504,8 @@ def main():
     config.elmo_style = args.elmo_style  # TODO: experiment weighting different layers(ELMo style)
     tokenizer = tokenizer_class.from_pretrained(args.tokenizer_name if args.tokenizer_name else args.model_name_or_path,
                                                 do_lower_case=args.do_lower_case)
-    model = model_class.from_pretrained(args.model_name_or_path, from_tf=bool('.ckpt' in args.model_name_or_path),
+    bp()
+    model = model_class.from_p1retrained(args.model_name_or_path, from_tf=bool('.ckpt' in args.model_name_or_path),
                                         config=config)
 
     if args.local_rank == 0:
